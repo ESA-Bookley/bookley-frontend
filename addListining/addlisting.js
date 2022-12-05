7bb2b2509b2546d68da8c7bd33bb8f3c704ff0c7
batoul <batoulhareb2020@gmail.com>
2022-12-05 18:34:46 +0200

    validationFormaddlistinginit2

--- addListining/addlisting.js
+++ addListining/addlisting.js
@@ -12,4 +12,28 @@ $(document).ready(function () {
             reader.readAsDataURL(file);
         }
     });
+    $('#add').on('click',function(){
+        const title=$('#title');
+        console.log(title);
+        const countpage=$('#countpage');
+        const lang=$('#lang');
+        const neww =$('#new');
+        const test1 = /([a-z].*[A-Z])|([A-Z].*[a-z])([0-9])+([!,%,&,@,#,$,^,*,?,_,~])/;
+        const test2 = /([!,%,&,@,#,$,^,*,?,_,~])/;
+        $('.error-message').hide().text('');
+        if(title.val()==""){
+            title.parents('.hold').find('.error-message').show().text('please fill out this field');   
+        }
+
+        if(countpage.val()==""){
+            countpage.parents('.hold').find('.error-message').show().text('please fill out this field');   
+        }else if (countpage.val()) {
+            if (test1.test(countpage.val()) == true || test2.test(countpage.val()) == true) {
+                countpage.parents('.hold').find('.error-message').show().text('this feild must be letter'); 
+            }
+        }
+        if (!(neww.prop("checked"))) {
+            neww.parents('.hold').find('.error-message').show().text('please fill out this field');  
+         }
+    });
 });
